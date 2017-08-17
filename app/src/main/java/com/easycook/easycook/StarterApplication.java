87b3e4ed394818af3b55b83a0f12f1c2d1fcb01f
Gabriel Tagliari Rodrigo <gabriel.tagliari93@gmail.com>
2017-08-16 21:33:36 -0300

    Parse configurado

---
+++ app/src/main/java/com/easycook/easycook/StarterApplication.java
@@ -0,0 +1,27 @@
+package com.easycook.easycook;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+
+/** Created by gabriel on 8/16/17. */
+public class StarterApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        // Add your initialization code here
+        Parse.initialize(this);
+
+        //ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+        // Optionally enable public read access.
+        //defaultACL.setPublicReadAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
+    }
+}
